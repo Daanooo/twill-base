2151d8fbd70110bb1c1505ca15c747e27d2fa96a
Daanooo <daankraaijeveld@icloud.com>
2023-03-09 16:43:10 +0100

    add click functionality

--- resources/views/site/layouts/header.blade.php
+++ resources/views/site/layouts/header.blade.php
@@ -14,30 +14,31 @@
             @endforeach
         </div>
 
-        <div class="space-y-1 md:hidden my-auto cursor-pointer mobile-nav-toggle inline-block">
-            <div class="w-5 h-0.5 bg-gray-600 mobile-nav-toggle"></div>
-            <div class="w-5 h-0.5 bg-gray-600 mobile-nav-toggle"></div>
-            <div class="w-5 h-0.5 bg-gray-600 mobile-nav-toggle"></div>
+        <div class="space-y-1 md:hidden my-auto cursor-pointer mobile-nav-toggle">
+            <div class="w-5 h-0.5 bg-gray-600"></div>
+            <div class="w-5 h-0.5 bg-gray-600"></div>
+            <div class="w-5 h-0.5 bg-gray-600"></div>
         </div>
     </nav>
 
-    <div class="h-full w-full bg-gray-100 fixed top-0 p-5 hidden" id="main-mobile-nav">
-        <div class="cursor-pointer absolute right-5 inline-block mobile-nav-toggle">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 block mobile-nav-toggle">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" class="mobile-nav-toggle" />
+    <div class="h-full w-full bg-gray-100 fixed top-0 p-5 mobile-nav-toggle hidden" id="main-mobile-nav">
+        <div class="cursor-pointer absolute right-5 inline-block">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 block">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
         </div>
     </div>
 </header>
 
 <script>
-    document.addEventListener('click', function(event) {
-        if (! event.target.matches('.mobile-nav-toggle')) {
-            return;
-        }
 
-        var mobileMenu = document.getElementById('main-mobile-nav');
+let mobileNavToggles = document.querySelectorAll('.mobile-nav-toggle');
+let mobileNavBar = document.getElementById('main-mobile-nav');
 
-        mobileMenu.classList.toggle('hidden');
-    })
+mobileNavToggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        mobileNavBar.classList.toggle('hidden');
+    });
+});
+
 </script>
